02dd25f259bdfb59ff48c1e32389ba9d3cd3939c
naveen0564 <naveen.makkapati@gmail.com>
2023-08-31 06:11:36 -0400

    1 file modified

--- index.js
+++ index.js
@@ -4,3 +4,81 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+// Program to find sum of elements in a given array
+
+const arr = [15, 12, 13, 10];
+const sum = arr.reduce((a,b)=>a+b,0);
+console.log(sum);
+
+function calSum(array, lenght) {
+ if(lenght<=0){
+   return 0;
+ }
+ return calSum(array, lenght-1) + array[lenght-1];
+}
+
+console.log(calSum(arr, arr.length));
+
+//For all Array elements find Product of Sum of all smaller and Sum of all greater elements
+
+let sumOf = [8, 4, 9, 3];
+let result=[];
+
+for(let i=0; i< sumOf.length; i++){
+  let sm=[];
+  let lg=[];
+  for(let j=0; j < sumOf.length; j++){
+    if(sumOf[i] !== sumOf[j]){
+      if(sumOf[i]>sumOf[j]){
+        sm.push(sumOf[j])
+      }else{
+        lg.push(sumOf[j])
+      }
+    }
+  }
+  console.log(sm, lg)
+  result.push(sm.reduce((a,b)=>a+b,0) * lg.reduce((a,b)=>a+b,0))
+}
+console.log(result);
+
+//For all Array elements find Product of Sum of all smaller and Sum of all greater elements using binary search
+
+function sortedArrayVal(array){
+let sortedArray = [...array];
+sortedArray.sort((a,b)=>a-b);
+console.log('sortedArray', sortedArray);
+let prefixArray = new Array().fill(0);
+console.log(prefixArray.length);
+prefixArray[0] = sortedArray[0];
+
+for(let i=1;i<sortedArray.length; i++){
+  prefixArray[i]= prefixArray[i-1] + sortedArray[i];
+}
+for(var i=0; i<sortedArray.length; i++){
+  const elemIndex = bSearch(sortedArray,0,sortedArray.length, array[i])
+  const sm = prefixArray[elemIndex]-array[i];
+  const lg =prefixArray[sortedArray.length-1]-prefixArray[elemIndex];
+  console.log(sm*lg);
+}
+console.log(prefixArray);
+}
+
+sortedArrayVal(sumOf);
+
+function bSearch(arra, low, high, ele){
+  if(high>=low){
+  const mid = Math.floor((high+low)/2);
+ // console.log('mid', mid);
+  if(arra[mid] === ele){
+    return mid;
+  }
+  else if(arra[mid]> ele){
+    return bSearch(arra, low, mid-1, ele);
+  }
+  else {
+    return bSearch(arra, mid+1, high, ele);
+  }
+  }
+}
+// console.log(bSearch([2,3,4,24], 0, 4, 24));
